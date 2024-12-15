c90a463768ad04aa4ac6ff2d1756bd6d39d14b3b
bhanufriend <akulabhanuprasadreddy@gmail.com>
2024-12-15 08:36:30 -0600

    argumentpractice

---
+++ agrumentpractice.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=1
+person2=2
+
+echo "$person1:: hi $person2 ,how areyou?"
+echo "$person2:: hello $person1. i am good then what about you?"
+echo "$person1:: i am good $person2what  is your going on?"
+echo "$person2:: i started learning java full stock $person1"
