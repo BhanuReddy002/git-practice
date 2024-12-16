e57a19e8542172bcaaf07a173b545939be9a924b
bhanufriend <akulabhanuprasadreddy@gmail.com>
2024-12-16 11:58:49 -0600

    arraypractice.sh

---
+++ arraypractice.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# index starts from 0 ,size is 3
+FRUITS=("apple","mango","orange") #array
+
+echo "first fruit is: ${FRUITS[0]}"
+echo "second fruit is: ${FRUITS[1]}"
+echo "third fruit is: ${FRUITS[2]}"
+
+echo "first fruit is: ${FRUITS[@]}"
