817106e707f77e334a1c8d06639678e29774dc75
bhanufriend <akulabhanuprasadreddy@gmail.com>
2024-12-16 11:38:47 -0600

    unseenpasswordvariables.sh

---
+++ unseenpassword.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#USERNAME=bhanu
+#PASSWORD=123
+ 
+ echo "please enter your username::"
+ read  -s USERNAME #-s is we cannot see name
+ echo "username entered is: $USERNAME"
+
+ echo "please enter password::"
+
+ read -s PASSWORD #-s we cannot see password
+
+ echo "password entered is: $PASSWORD"
