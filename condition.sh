a31e213be09527d182a35f6b9f19b57199219b90
bhanufriend <akulabhanuprasadreddy@gmail.com>
2024-12-17 15:16:50 -0600

    condition variables.sh

---
+++ condition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+get the number
+check it is greater than 20 or not
+
+
+NUMBER=$1
+
+if [ $NUMBER -gt 20 ] #gt,lt,eq,
+then 
+    echo "given number: $NUMBER is greater than 20"
+else
+    echo "given number: $NUMBER is less than 20"
+fi        
