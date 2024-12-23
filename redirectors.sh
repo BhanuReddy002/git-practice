45c91e3e608230b2c63390dab681481a14299675
bhanufriend <akulabhanuprasadreddy@gmail.com>
2024-12-23 15:11:05 -0600

    redirectors.sh

--- redirectors.sh
+++ redirectors.sh
@@ -34,10 +34,10 @@ VALIDATE(){
 
 CHECK_ROOT
 
- dnf list installed $package &>>&>>$LOG_FILE 
+ dnf list installed $package &>>$LOG_FILE 
    if [ $? -ne 0 ]
    then 
-       echo "$package is not installed, going to install it.." &>>&>>$LOG_FILE 
+       echo "$package is not installed, going to install it.." &>>$LOG_FILE 
        dnf install $package -y &>>$LOG_FILE 
        VALIDATE $? "Insatlling $package"
     else   
