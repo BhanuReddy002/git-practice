6767497b514d8d1b66b31673c475cb88d2e5df14
bhanufriend <akulabhanuprasadreddy@gmail.com>
2024-12-23 12:03:36 -0600

    loops2.sh

--- loops2.sh
+++ loops2.sh
@@ -30,7 +30,17 @@ CHECK_ROOT
 #sh lops2.sh git mysql postfix ngnix
 for package in $@    # $@ refers to all arguments passed to it
 do 
-  echo $package
+   
+   dnf list installed $package
+   if [ $? -ne 0]
+   then 
+       echo "package is not installed, going to install it.."
+       dnf install $package -y
+       VALIDATE $? "Insatlling $package"
+    else   
+         echo "$package is already installed..nothing to do"
+    fi     
+
 done  
 
  
